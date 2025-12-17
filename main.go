a083a82203c31864c5cf940e1a8c1a62106d31e1
nilam <nilammartaanggreni@gmail.com>
2025-12-17 15:31:38 +0800

    Assignment 1: Create Simple task Management

--- main.go
+++ main.go
@@ -53,73 +53,94 @@ func validateNewTask() string {
 
 		if inputTask == "" {
 			fmt.Println("Please input the correct task")
+			continue
 		} else {
 			return inputTask
 		}
 	}
 }
 
-func validateMarkInput(length int) int {
+func validateChooseTask(length int, ask string) int {
+	var inputTask int
+	
 	for {
-		fmt.Print("Choose the task number to be marked: ")
-		var inputMark int
-		fmt.Scanf("%d", &inputMark)
+		fmt.Print(ask)
+		fmt.Scanf("%d", &inputTask)
 
-		if inputMark > length {
-			fmt.Println("Please input the correct task")
+		if inputTask <= length && inputTask >= 1 {
+			return inputTask - 1
 		} else {
-			return inputMark
+			fmt.Println("Please choose the correct task")
+			continue
 		}
 	}
 }
 
 func main() {
-	var menu int = 0
+	var menu string = ""
+	// inisiasi slice
 	var tasks []Task
 
 	for {
 		showMenu()
-		fmt.Scanf("%d", &menu)
+		fmt.Scanf("%s", &menu)
 
-		if menu == 1 {
+		if menu == "1" {
 			fmt.Println("===== SHOW TASKS =====")
 
+			// Show Tasks
 			showTasks(tasks)
 			fmt.Println("")
 
 			pressEnterToContinue()
 			continue
 
-		} else if menu == 2 {
+		} else if menu == "2" {
 			fmt.Println("===== ADD TASK =====")
 
-			var inputTask string = validateNewTask()
+			// Validate the input task
+			var inputTask = validateNewTask()
+			// Create new task
 			var newTask = Task{inputTask, false}
+			// Save the new task
 			tasks = append(tasks, newTask)
 
 			fmt.Println(" ")
 			pressEnterToContinue()
 			continue
 
-		} else if menu == 3 {
+		} else if menu == "3" {
 			fmt.Println("===== MARK TASK =====")
 
+			// Show Task
 			showTasks(tasks)
 			fmt.Println("")
 
-			// validate and mark task
-			inputMark := validateMarkInput(len(tasks))
+			// Validate the input task
+			inputTask := validateChooseTask(len(tasks), "Choose the task number to be marked: ")
 
-			tasks[inputMark-1].Done = true
-
-			fmt.Println("Task has been marked successfully!")
+			// Mark task
+			tasks[inputTask].Done = true
+			fmt.Println("Task marked successfully!")
 
 			fmt.Println(" ")
 			pressEnterToContinue()
-		} else if menu == 4 {
+		} else if menu == "4" {
 			fmt.Println("===== DELETE TASK =====")
 
-		} else if menu == 5 {
+			// Show Task
+			showTasks(tasks)
+
+			// Validate the input task
+			inputTask := validateChooseTask(len(tasks), "Choose the task number to be deleted: ")
+
+			// Delete Task
+			tasks = append(tasks[:inputTask], tasks[inputTask+1:]...)
+			fmt.Println("Task has been deleted!")
+
+			fmt.Println(" ")
+			pressEnterToContinue()
+		} else if menu == "5" {
 			fmt.Println("===== EXIT =====")
 			fmt.Println("See you next time!")
 			break
